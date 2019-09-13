6062bd5317dacaad4b6380d5f232b4d6a79062a7
trongthoai <thoaithieu.pttt@gmail.com>
2019-09-13 21:51:18 +0700

    searchbar + drawer

--- app/src/main/java/com/example/fahasa/View/TrangChu/TrangChuActivity.java
+++ app/src/main/java/com/example/fahasa/View/TrangChu/TrangChuActivity.java
@@ -15,8 +15,8 @@ import com.example.fahasa.R;
 import com.google.android.material.tabs.TabLayout;
 
 public class TrangChuActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    TabLayout tabLayout;
+
+
     ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,26 +25,16 @@ public class TrangChuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trangchu_layout);
 
-        toolbar = findViewById(R.id.toolbar);
-        tabLayout = findViewById(R.id.tabs);
-        viewPager = findViewById(R.id.viewpager);
-
-        setSupportActionBar(toolbar);
-
-        tabLayout.setupWithViewPager(viewPager);
 
 
 
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menutrangchu,menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        return super.onOptionsItemSelected(item);
+
+
+
+
+
+
+
     }
 
 }
